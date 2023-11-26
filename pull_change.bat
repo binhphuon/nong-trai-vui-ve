f3ba8be999514b371f7862f02f0ee18c8c22c5bc
binhphuon <danghoangbinh2007@gmail.com>
2023-11-26 12:55:35 +0700

    Cß║¡p nhß║¡t tß╗▒ ─æß╗Öng

---
+++ pull_change.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\DuongPham\Desktop\nong-trai-vui-ve"
+git pull
--- push_changes.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "đường_dẫn_tới_thư_mục_dự_án_git_của_bạn"
-git add .
-git commit -m "Cập nhật tự động"
-git push
