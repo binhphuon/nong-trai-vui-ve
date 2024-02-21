e029c8b7107556dd4f425c2c23ea1f854a1aa10d
binhphuon <danghoangbinh2007@gmail.com>
2024-02-21 18:18:42 +0700

    auto update

--- pip install bla bla.bat
+++ pip install bla bla.bat
@@ -1,6 +1,6 @@
 @echo off
-pip install requests selenium>=4.15.2 ipapi undetected-chromedriver selenium-wire pandas numpy>=1.22.2 setuptools>=69.0.2 psutil
-pip install --upgrade requests selenium>=4.15.2 ipapi undetected-chromedriver selenium-wire pandas numpy>=1.22.2 setuptools>=69.0.2 psutil
+pip install requests selenium>=4.15.2 ipapi undetected-chromedriver==3.5.5 selenium-wire pandas numpy>=1.22.2 setuptools>=69.0.2 psutil
+pip install --upgrade requests selenium>=4.15.2 ipapi undetected-chromedriver==3.5.5 selenium-wire pandas numpy>=1.22.2 setuptools>=69.0.2 psutil
 
 
 
